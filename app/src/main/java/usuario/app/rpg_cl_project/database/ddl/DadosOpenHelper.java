82d02eb26705e0bfacbdd34cba7e87d5f363f932
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-04 11:46:10 -0300

    Definida estrutura da classe DadosOpenHelper, principal classe que permitira a manipulacao do banco de dados

--- app/src/main/java/usuario/app/rpg_cl_project/database/ddl/DadosOpenHelper.java
+++ app/src/main/java/usuario/app/rpg_cl_project/database/ddl/DadosOpenHelper.java
@@ -1,4 +1,25 @@
 package usuario.app.rpg_cl_project.database.ddl;
 
-public class DadosOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DadosOpenHelper extends SQLiteOpenHelper {
+    //Construtor
+    public DadosOpenHelper(@Nullable Context context) {
+        super(context, "BD_RPG_CL_PROJECT", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //db.execSQL(ScriptDDL.getCreateTableCalculo());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Método para realizar qualquer atualização no BD, como incluir um campo numa tabela, por exemplo
+
+    }
 }
