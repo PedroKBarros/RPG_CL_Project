183e3d5bee1b709b7169c69af2744bc8b52fde8d
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-08 16:45:19 -0300

    Atualizado código da MainActivity para transformacao da activity_main em uma splash screen, com timer

--- app/src/main/java/usuario/app/rpg_cl_project/MainActivity.java
+++ app/src/main/java/usuario/app/rpg_cl_project/MainActivity.java
@@ -1,57 +1,38 @@
 package usuario.app.rpg_cl_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import usuario.app.rpg_cl_project.database.ddl.DadosOpenHelper;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    MainActivity activityAtual;
-    TextView txtViewAuthor;
-    private SQLiteDatabase conexao;
-    private DadosOpenHelper dadosOpenHelper;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        activityAtual = this;
 
-        new Thread(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash screen com um timer.
+             */
+            @Override
             public void run() {
-                try {
-                    establishesDBConnection();
-                    activityAtual.runOnUiThread(new Runnable() {
-                        public void run() {
-                            mostraMensagemToast("Conexão com BD estabelicida!", Toast.LENGTH_SHORT);
-                        }
-                    });
-                    dadosOpenHelper.fechaConexao();
-                }catch(android.database.SQLException e){
-                    //Há dois SQLException e temos q usar o android.database, pois é o q pertence ao pacote do SQLite
-                    mostraMensagemToast(e.getMessage(), Toast.LENGTH_SHORT);
-                }finally {
-                    dadosOpenHelper.fechaConexao();
-                }
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity menu
+                Intent intent = new Intent(MainActivity.this,
+                        MenuAcitivity.class);
+                startActivity(intent);
+                // Fecha esta activity
+                finish();
             }
-        }).start();
+        }, SPLASH_TIME_OUT);
+
+
     }
 
-    private void establishesDBConnection() throws android.database.SQLException{
-        //Método que vai criar a conexão com o BD em si. Esse método não cria nenhuma estrutura, apenas conecta com o BD
-        dadosOpenHelper = new DadosOpenHelper(this); //Lembre-se que nosso contrutor pede o contexto, ou seja, a Activity em questão
-        //Criando conexão
-        conexao = dadosOpenHelper.estabeleceConexao(); //O writable permite leitura e escrita, enquanto q o redtable, só leitura
-        //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente);
-    }
 
-    private void mostraMensagemToast(String mensagem, int duracao){
-        Toast toast = Toast.makeText(this, mensagem, duracao);
-        toast.show();
-    }
 }
