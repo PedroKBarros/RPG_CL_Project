207685ad906857e47f74c283b5834de8134ac555
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-11 12:23:28 -0300

    Desenvolvido metodo para retorno de todas as tuplas de configuracoes do app e criado construtor do repositorio AppSettingRepository.

--- app/src/main/java/usuario/app/rpg_cl_project/database/dml/repositorios/AppSettingRepository.java
+++ app/src/main/java/usuario/app/rpg_cl_project/database/dml/repositorios/AppSettingRepository.java
@@ -6,38 +6,41 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AppConfigurationRepository {
+import usuario.app.rpg_cl_project.dominio.AppSetting;
+import usuario.app.rpg_cl_project.dominio.GeneralSetting;
+
+public class AppSettingRepository {
     private SQLiteDatabase conexao;
 
-    public AppConfigurationRepository(SQLiteDatabase conexao){ this.conexao = conexao; }
+    public AppSettingRepository(SQLiteDatabase conexao){ this.conexao = conexao; }
 
-    public List<Calculo> queryAllTuples(){
-        List<Calculo> calculos = new ArrayList<Calculo>();
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT NOME, EXPRESSAO, DATA, DESCRICAO ");
-        sql.append("FROM CALCULO");
+    public AppSetting queryAllTuples(){
+        List<GeneralSetting> generalSettings = new ArrayList<GeneralSetting>();
+        AppSetting appSetting = new AppSetting();
+        String chave;
+        int valor, valorMin, valorMax;
 
-        Cursor resultado = conexao.rawQuery(sql.toString(), null); //Método para escrever consulta SQL, passando a String da consulta e parâmetros da consulta em um vetor de Strings
+        Cursor cursor = conexao.rawQuery(ScriptDML.getQueryAppSettingAllTuples(), null); //Método para escrever consulta SQL, passando a String da consulta e parâmetros da consulta em um vetor de Strings
         //Verificando se algum registro foi retornado:                         //O primeiro parâmetro é a quary e o outro são os argumentos. Porém, como não temos argumentos, pois não usamos a clausula where, coloquei como null.
-        if (resultado.getCount() > 0){                                           //Ele retorna um objeto Cursor, que contém as tuplas de resultado. Vamos converter para List<Cliente>.
+        if (cursor.getCount() > 0){                                           //Ele retorna um objeto Cursor, que contém as tuplas de resultado. Vamos converter para List<Cliente>.
             //Garantindo q a gnt vai ter os resultados a
             // partir do  primeiro registro:
-            resultado.moveToFirst();
-            //Transformando o resultado em List<Cliente>:
+            cursor.moveToFirst();
+            //Transformando o resultado em List<GeneralSetting>:
             do{
-                //Convertendo os valores da tabela em atributos da classe Calculo:
-                Calculo calculo = new Calculo();
-                calculo.nome = resultado.getString(resultado.getColumnIndexOrThrow("NOME"));
-                calculo.expressao = resultado.getString(resultado.getColumnIndexOrThrow("EXPRESSAO"));
-                calculo.data = resultado.getString(resultado.getColumnIndexOrThrow("DATA"));
-                calculo.descricao = resultado.getString(resultado.getColumnIndexOrThrow("DESCRICAO"));
+                //Convertendo os valores da tabela em atributos da classe GeneralSetting
+                chave = cursor.getString(cursor.getColumnIndex("chave"));
+                valor = cursor.getInt(cursor.getColumnIndex("valor"));
+                valorMin = cursor.getInt(cursor.getColumnIndex("valor_min"));
+                valorMax = cursor.getInt(cursor.getColumnIndex("valor_max"));
+                GeneralSetting generalSetting = new GeneralSetting(chave, valor, valorMin, valorMax);
 
-                calculos.add(calculo);
-            }while(resultado.moveToNext());//Enquanto houver registro,
+                generalSettings.add(generalSetting);
+            }while(cursor.moveToNext());//Enquanto houver registro,
             // vamos mover o resultado para o próximo registro. Esse método retorna true, caso tenha conseguido mover.
         }
 
-
-        return calculos;
+        appSetting.setGeneralSettings(generalSettings);
+        return appSetting;
     }
 }
