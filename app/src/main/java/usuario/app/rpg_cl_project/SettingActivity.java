2477de68800cf8f46fa518dc3801fcca2336ab8a
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-11 17:31:21 -0300

    Atualizada acitivity_general_setting e iniciado desenvolvimento da classe SettingActivity.

--- app/src/main/java/usuario/app/rpg_cl_project/SettingActivity.java
+++ app/src/main/java/usuario/app/rpg_cl_project/SettingActivity.java
@@ -1,14 +1,22 @@
 package usuario.app.rpg_cl_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_general_setting);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(
+                                                                R.id.complete_header_out_play);
+        TextView headerTitle = (TextView) constraintLayout.getViewById(R.id.txt_header_title);
+        headerTitle.setText("CONFIGURAÇÕES");
+
     }
 }
