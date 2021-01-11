8d66bdd9ca5ee4f1e562b0704f3d022b1c8b295c
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-11 12:22:25 -0300

    Atualizado codigo das classes AppSetting e GeneralSetting

--- app/src/main/java/usuario/app/rpg_cl_project/dominio/AppSetting.java
+++ app/src/main/java/usuario/app/rpg_cl_project/dominio/AppSetting.java
@@ -1,5 +1,20 @@
 package usuario.app.rpg_cl_project.dominio;
 
-public class AppConfiguration {
-    private List<GeneralSetting> 
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppSetting {
+    private List<GeneralSetting> generalSettings;
+
+    public AppSetting(){
+        this.generalSettings = new ArrayList<GeneralSetting>();
+    }
+
+    public List<GeneralSetting> getGeneralSettings() {
+        return generalSettings;
+    }
+
+    public void setGeneralSettings(List<GeneralSetting> generalSettings) {
+        this.generalSettings = generalSettings;
+    }
 }
--- app/src/main/java/usuario/app/rpg_cl_project/dominio/GeneralSetting.java
+++ app/src/main/java/usuario/app/rpg_cl_project/dominio/GeneralSetting.java
@@ -1,4 +1,17 @@
 package usuario.app.rpg_cl_project.dominio;
 
-public class GeneralConfiguration {
+public class GeneralSetting {
+    private String chave;
+    private int valor;
+    private int valorMin;
+    private int valorMax;
+
+    public GeneralSetting(String chave, int valor, int valorMin, int valorMax){
+        this.chave = chave;
+        this.valor = valor;
+        this.valorMin = valorMin;
+        this.valorMax = valorMax;
+    }
+
+
 }
