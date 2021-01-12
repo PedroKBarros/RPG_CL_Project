ea3cb552970fcab26f8951af644fa6a525df352c
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-12 16:34:23 -0300

    Desenvolvido metodo, para insercao de tupla, da classe RepositorioTbConfigApp.

--- app/src/main/java/usuario/app/rpg_cl_project/database/dml/repositorios/RepositorioTbConfigApp.java
+++ app/src/main/java/usuario/app/rpg_cl_project/database/dml/repositorios/RepositorioTbConfigApp.java
@@ -1,5 +1,6 @@
 package usuario.app.rpg_cl_project.database.dml.repositorios;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,18 @@ public class RepositorioTbConfigApp {
     private SQLiteDatabase conexao;
 
     public RepositorioTbConfigApp(SQLiteDatabase conexao){ this.conexao = conexao; }
+
+    public void inserirTupla(ConfiguracaoGeral configuracaoGeral){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("chave", configuracaoGeral.getChave());
+        contentValues.put("valor_min", configuracaoGeral.getValorMin());
+        contentValues.put("valor_max", configuracaoGeral.getValorMax());
+        contentValues.put("valor", configuracaoGeral.getValor());
+
+        conexao.insertOrThrow("TB_CONFIG_APP", null, contentValues); //Esse método insere os dados no BD, podendo gerar uma exceção caso ocorra um erro, e se não ocorrer, retorna
+        //o valor da chave primária, o q é bom, pois podemos usá-lo caso seja necessário
+        //O primeiro parâmetro é o nome da tabela, o segundo é besteira e o terceiro são os valores da tupla.
+    }
 
     public List<ConfiguracaoGeral> buscarTodasTuplas(){
         List<ConfiguracaoGeral> configuracoesGerais = new ArrayList<ConfiguracaoGeral>();
