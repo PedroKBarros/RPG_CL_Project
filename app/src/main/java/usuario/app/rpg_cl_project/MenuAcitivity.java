75ddb1a81e57c07a9fc081328b737bfd206db2d4
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-12 20:21:48 -0300

    Adiciado diretorio de recurso raw com um arquivo mp3 para representacao do som de clique em botoes. Alem disso, desenvolvido som dos botoes que de sair e configurar do menu.

--- app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
+++ app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
@@ -3,7 +3,10 @@ package usuario.app.rpg_cl_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaActionSound;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,10 +22,12 @@ public class MenuAcitivity extends AppCompatActivity {
 
         botaoConfig = (Button) findViewById(R.id.bt_config);
         botaoSair = (Button) findViewById(R.id.bt_sair);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.som_clique_botao);
 
         botaoConfig.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                mediaPlayer.start();
                 Intent intent = new Intent(getBaseContext(), ConfiguracaoAppActivity.class);
                 startActivity(intent);
             }
@@ -30,6 +35,7 @@ public class MenuAcitivity extends AppCompatActivity {
         botaoSair.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                mediaPlayer.start();
                 finishAffinity(); //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente)
             }
         });
