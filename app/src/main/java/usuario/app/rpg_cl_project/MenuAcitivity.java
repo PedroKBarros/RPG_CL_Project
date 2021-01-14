0d543bd88d7dd8d8a302a3c18bc63f5c7372e4b9
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-13 21:09:05 -0300

    Implementacao da classe ExecutaAudio na classe MenuActivity para execucao do som dos botoes 'SAIR' e 'CONFIG'.

--- app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
+++ app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
@@ -3,17 +3,25 @@ package usuario.app.rpg_cl_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaActionSound;
+import android.database.SQLException;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import usuario.app.rpg_cl_project.auxiliar.ExecutaAudio;
+import usuario.app.rpg_cl_project.dominio.ConfiguracaoApp;
+import usuario.app.rpg_cl_project.dominio.ConfiguracaoGeral;
 
 public class MenuAcitivity extends AppCompatActivity {
 
     private Button botaoConfig;
     private Button botaoSair;
+    private ExecutaAudio executaAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +30,13 @@ public class MenuAcitivity extends AppCompatActivity {
 
         botaoConfig = (Button) findViewById(R.id.bt_config);
         botaoSair = (Button) findViewById(R.id.bt_sair);
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.som_clique_botao);
+
+        executaAudio = new ExecutaAudio(this);
 
         botaoConfig.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                mediaPlayer.start();
+                executaSomBotao();
                 Intent intent = new Intent(getBaseContext(), ConfiguracaoAppActivity.class);
                 startActivity(intent);
             }
@@ -35,9 +44,29 @@ public class MenuAcitivity extends AppCompatActivity {
         botaoSair.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                mediaPlayer.start();
+                executaSomBotao();
                 finishAffinity(); //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente)
             }
         });
     }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        this.liberaRecursos();
+    }
+
+    private void liberaRecursos(){
+        executaAudio.liberaRecursos();
+    }
+
+    private void executaSomBotao(){
+        new Thread(new Runnable() {
+            public void run() {
+                executaAudio.executaAudioAsync(ExecutaAudio.URI_SOM_CLIQUE_BOTAO);
+            }
+        }).start();
+    }
+
 }
