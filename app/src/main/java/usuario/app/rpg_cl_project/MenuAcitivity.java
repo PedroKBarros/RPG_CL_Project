67cf675f7c0652600af523e34a1ecbd277e94bb8
Pedro Barros <pedrokatsbarros@gmail.com>
2021-01-17 01:43:06 -0300

    Refatorando codigo da classe MenuActivity para torna-lo mais organizado, distribuindo melhor os codigos em metodos, modularizando, portanto, o codigo. Alem disso, foi adicionado a confirmacao ao se clicar no botao 'sair' do menu.

--- app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
+++ app/src/main/java/usuario/app/rpg_cl_project/MenuAcitivity.java
@@ -2,6 +2,7 @@ package usuario.app.rpg_cl_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.SQLException;
 import android.media.MediaPlayer;
@@ -14,6 +15,7 @@ import android.widget.Toast;
 import java.io.IOException;
 
 import usuario.app.rpg_cl_project.auxiliar.ExecutaAudio;
+import usuario.app.rpg_cl_project.auxiliar.ExecutaMensagem;
 import usuario.app.rpg_cl_project.dominio.ConfiguracaoApp;
 import usuario.app.rpg_cl_project.dominio.ConfiguracaoGeral;
 
@@ -23,18 +25,43 @@ public class MenuAcitivity extends AppCompatActivity {
     private Button botaoSair;
     private ExecutaAudio execSomCliqueBotoes;
     private ExecutaAudio execMusicaFundo;
+    private ExecutaMensagem execMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        botaoConfig = (Button) findViewById(R.id.bt_config);
-        botaoSair = (Button) findViewById(R.id.bt_sair);
+        this.inicializaAtributos();
 
-        execSomCliqueBotoes = new ExecutaAudio(this);
-        execMusicaFundo = new ExecutaAudio(this);
+        this.defineEventoClickBotaoConfig();
+        this.defineEventoClickBotaoSair();
+    }
 
+    private void defineEventoClickBotaoSair(){
+        botaoSair.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                execSomCliqueBotoes.executaAudioThreadUI(ExecutaAudio.URI_SOM_CLIQUE_BOTAO, false);
+                execMensagem.criaAlertDialogComTitulo("Sair", "Deseja realmente sair?");
+                execMensagem.addBotaoAlertDialog(ExecutaMensagem.ALERTDLG_TIPO_BOTAO_POSITIVO,
+                        "SIM", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finishAffinity(); //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente)
+                            }
+                        });
+                execMensagem.addBotaoAlertDialog(ExecutaMensagem.ALERTDLG_TIPO_BOTAO_NEGATIVO,
+                        "NÃO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) { }
+                        });
+                execMensagem.mostraAlertDialog();
+            }
+        });
+    }
+
+    private void defineEventoClickBotaoConfig(){
         botaoConfig.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -44,13 +71,14 @@ public class MenuAcitivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        botaoSair.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                execSomCliqueBotoes.executaAudioThreadUI(ExecutaAudio.URI_SOM_CLIQUE_BOTAO, false);
-                finishAffinity(); //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente)
-            }
-        });
+    }
+
+    private void inicializaAtributos(){
+        botaoConfig = (Button) findViewById(R.id.bt_config);
+        botaoSair = (Button) findViewById(R.id.bt_sair);
+        execSomCliqueBotoes = new ExecutaAudio(this);
+        execMusicaFundo = new ExecutaAudio(this);
+        execMensagem = new ExecutaMensagem(this);
     }
 
     @Override
