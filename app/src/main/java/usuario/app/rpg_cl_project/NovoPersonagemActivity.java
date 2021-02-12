476b2278d5af5e48674de410b70041e7023d64c9
Pedro Barros <pedrokatsbarros@gmail.com>
2021-02-12 17:40:47 -0300

    Adicionada activity para criacao de novo personagem.

---
+++ app/src/main/java/usuario/app/rpg_cl_project/NovoPersonagemActivity.java
@@ -0,0 +1,14 @@
+package usuario.app.rpg_cl_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovoPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_personagem);
+    }
+}
