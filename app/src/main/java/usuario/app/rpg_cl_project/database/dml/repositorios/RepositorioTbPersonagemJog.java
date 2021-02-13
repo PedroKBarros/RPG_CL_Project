9a240f34abcb065d18bcccd8794610cabf8f6cee
Pedro Barros <pedrokatsbarros@gmail.com>
2021-02-13 17:47:37 -0300

    Adicionada classe repositorio responsavel por realizar consultas a tabela 'TB_PERSONAGEM_JOG'.

---
+++ app/src/main/java/usuario/app/rpg_cl_project/database/dml/repositorios/RepositorioTbPersonagemJog.java
@@ -0,0 +1,10 @@
+package usuario.app.rpg_cl_project.database.dml.repositorios;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class RepositorioTbPersonagemJog {
+
+    private SQLiteDatabase conexao;
+
+    public RepositorioTbPersonagemJog(SQLiteDatabase conexao){ this.conexao = conexao; }
+}
